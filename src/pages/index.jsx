b838f488e3dd67f039264e2529ae4bde99eb1132
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2024-06-07 15:17:39 -0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,2 +1,16 @@
 import Image from "next/image";
 import styled from "styled-components";
+
+export default function Home(){
+    return (
+        <>
+        <Head>
+          <title>Portfólio - Jhonatan</title>
+          <meta name="keywords" content="Lamborghini, models, cars" />
+        </Head>
+          <StyledApresentacao>
+            <h1>Olá meu nome é Jhonatan</h1>
+          </StyledApresentacao>
+        </>
+      );
+    }
